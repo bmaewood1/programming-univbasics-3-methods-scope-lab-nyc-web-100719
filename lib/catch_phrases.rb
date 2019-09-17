02e2f9f8f240bed18ff5f78ac40f1ce3bf9595ca
Bailey Wood <woodbmae@gmail.com>
2019-09-17 14:14:00 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -8,17 +8,13 @@ def toadstool
   puts status
 end
 
-
 def link
   phrase = "It's Dangerous To Go Alone! Take This."
   puts phrase
 end
 
-describe "any_phrase" do
-  it "takes in an argument and puts out the catch phrase" do
-    phrase = "Do A Barrel Roll!"
-    expect{any_phrase(phrase)}.to output("Do A Barrel Roll!\n").to_stdout
-  end
+def any_phrase(phrase)
+  phrase = "Do A Barrel Roll!"
 end
 
-def any_phrase()
+
