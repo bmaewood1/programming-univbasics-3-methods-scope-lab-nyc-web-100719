5e1189628226ddf728d489189d9073b222b83b1d
Bailey Wood <woodbmae@gmail.com>
2019-09-17 14:12:16 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,15 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+describe "link" do
+  it "puts out 'It's Dangerous To Go Alone! Take This.'" do
+    expect{link}.to output("It's Dangerous To Go Alone! Take This.\n").to_stdout
+  end
+end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
